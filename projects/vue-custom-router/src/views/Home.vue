ca3d3c2f5a8a77e5bc258b000059cfc38ae38c0c
wuweihui <wuweihui9833@dingtalk.com>
2020-12-15 11:36:59 +0800

    修改显示文本

--- projects/vue-custom-router/src/views/Home.vue
+++ projects/vue-custom-router/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Primary App"/>
+    <HelloWorld msg="Welcome to Your Custom App"/>
   </div>
 </template>
 
